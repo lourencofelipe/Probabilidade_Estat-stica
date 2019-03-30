33b2da1a5b0e3387f611545bc15891741669b757
Samael Pereira Simoes <samael.simoes@gmail.com>
2019-03-30 17:13:04 -0300

    Ajustado logica e calculo de montagem da tabela

--- js/events.js
+++ js/events.js
@@ -1,29 +1,32 @@
+
+vlhtml    = "";
+contador  = 0;
+totalfi   = 0;
+totalxifi = 0;
+
 $( document ).ready(function() {
     bootbox.prompt("Informe a quantidade de Linhas! ", function( linhas ) { 
         let msg = validate( " Quantidade", linhas, 'linhas' );
         
         if( msg ) {           
-            modals( msg, "alert" );
+            modals( "Campos Inválidos: <br>" + msg, "alert" );
         } else {
-            montaTabela( linhas );
+            montaTabela( linhas, 'chamada' );
         }            
     });
 });
 
 function validate( campo, valor, tp ) {
 
-    let msg = ""
+    let msg = "";
 
-    if( valor == null ||  valor.trim() == "" ) {
-        msg += "-" + campo + " Está Vazio. </br>";
+    if( valor == null ||  valor.trim() == "" ) {        
+        msg += "-" + campo + " Está Vazio. <br>";
     }
+
     if( valor <= 0 ) {
-        msg += "-" + campo + " Valor invalido, deve ser maior que zero";
-    }
-
-    if( valor > 4 && tp == "linhas" ) {
-        msg += "-" + campo + " Valor de linhas invalido, <br> deve ser menor ou igual a 4";
-    }
+        msg += "-" + campo + " Valor invalido, deve ser maior que zero <br>";
+    }  
 
     return msg;
 }
@@ -44,14 +47,25 @@ function modals( msg, tpModal ) {
 function vlValores( tpDados ) {
     let msg = "";
 
-    msg += tpDados == "maximo" ? validate("Maximo: ", tpDados )  : "";
-    msg += tpDados == "minimo" ? validate("Minimo: " , tpDados ) : "";
+    if( tpDados == "minimo" ) {
+        msg += nMinimo();
+    } else if( tpDados == "maximo" ) {
+        msg += nMaximo();
+    }
 
     if( msg ) {
-        modals( msg, "" );
-    } else {
-        $('#valorMaximoMinimo').modal('hide');
+        modals( "Os seguintes campos estão inválidos: <br>" +  msg, "" );
     }
+}
+
+function nMaximo() {
+    return validate("Maximo: ", $("#vlmaximo").val());
+}
+function nMinimo() {
+    return validate("Minimo: " , $("#vlminimo").val());
+}
+function nFi() {
+    return validate("Fi: ", $("#vlFi").val());
 }
 
 function vlDados() {
@@ -59,44 +73,147 @@ function vlDados() {
 
     msg += tpDados == "maximo" ? validate("Maximo: ", tpDados ) : "";
     msg += tpDados == "minimo" ? validate("Minimo: " , tpDados ) : "";
-
     if( msg ) {
-        modals( msg, "" );
+        modals( "Campos Inválidos: <br>" + msg, "" );
     } else {
         $('#valorMaximoMinimo').modal('hide');
     }
 }
 
-async function montaTabela( totalLinhas ) {
 
-    let i    = 0, 
-        html = "",
-        msg  = "";
-    
-    if( totalLinhas ) {
+function calculaXi() {
+    let calculo = parseInt( $("#vlminimo").val() ) + parseInt( $("#vlmaximo").val());
+    return calculo / 2;
+}
+
+function calculadxifi( vl ) {
+    return vl *  $("#vlFi").val();
+}
+
+async function montaTabela( totalLinhas, tpChamada, html, i, proxChamada ) {
+       
+    qvlhtml   = html ? html : "";
+    contador = i ? i : "";
+
+    let    msg      = "";
+
+    if( tpChamada == "chamada" ) {
         $("#escondeLinhas").val( totalLinhas );
-        i++;
         $('#valorMaximoMinimo').modal('show');
-        
+        return;
     }
 
-    /*for( let i = 0; i <= linhas; i++ ) {
-        console.log(i);
-        chamadaModalPersonalisado();
+    contador = $("#contador").val() ? parseInt( $("#contador").val() )+1 : contador+1;
 
-        valMinimo = $("#vlminimo").val();
-        valMaximo = $("#vlmaximo").val();
+    if( proxChamada == "proxima" ) {
 
-        
-        msg += validate("Valor minimo: ", modals, "" );
-        msg += validate("Valor maximo: ", modals, "" );
+        $("#vlminimo").val("");
+        $("#vlmaximo").val("");
+        $("#vlFi").val("");
+        if( contador <= $("#escondeLinhas").val() ) { 
+            $('#valorMaximoMinimo').modal('show');
+        }
+        return;
+    }
 
-        html += "<tr>" +
 
-                "</tr>";
-    }*/
+    if( tpChamada == "retorno" ) {   
+       
+        msg += nMinimo();    
+        msg += nMaximo();        
 
-    $("#dadosTabela").html(html);
+        if( msg ) {
+            $('#valorMaximoMinimo').modal('show');
+            modals( "Os seguintes campos estão inválidos: <br>" +  msg, "" );        
+        } else if( contador <= $("#escondeLinhas").val() ) {
 
-    
+            $("#contador").val( contador );
+
+            let xi    = calculaXi();
+            let xifi  = calculadxifi(xi);
+
+            totalfi   = totalfi + parseInt( $("#vlFi").val() );
+            totalxifi = totalxifi + xifi;
+            
+            vlhtml += "<tr>" +
+                        "<td>"+
+                            "<table>"+
+                                "<tr>" + 
+                                    "<td>" +
+                                        $("#vlminimo").val()+
+                                    "</td>"+
+                                    "<td>" +
+                                        "<img src='img/simbulo.png' alt='Inclui numero a esquerda e excluo o numero da direita' style='height: 20px; width: auto;'>"+
+                                    "</td>"+
+                                    "<td>" +
+                                        $("#vlmaximo").val()+
+                                    "</td>"+
+                                "</tr>"+
+                            "</table>"+
+                        "</td>"+
+                        "<td>"+
+                            $("#vlFi").val()+
+                        "</td>"+
+                        "<td>"+
+                            xi+
+                        "</td>"+
+                        "<td>"+
+                            xifi+
+                        "</td>"+
+                    "</tr>";
+            /**
+             * Colocar modal aqui criar só uma TR etd 
+             * moda bruta criar só uma TR etd
+             * moda conforme zczuber criar só uma TR etd
+             */
+            if(  contador == $("#escondeLinhas").val() ) {
+                vlhtml  +=  "<tr>"+
+                                "<td>"+
+                                    "Total:"+
+                                "</td>"+
+                                "<td>"+
+                                    "E="+totalfi+
+                                "</td>"+
+                                "<td>"+
+                                    
+                                "</td>"+
+                                "<td>"+
+                                    "E="+totalxifi+
+                                "</td>"+
+                            "</tr>"+
+                            "<tr>"+
+                                "<td>"+
+                                    "Mediana: "+
+                                "</td>"+
+                                "<td>"+                                    
+                                    totalxifi / totalfi
+                                "</td>"+
+                                "<td>"+
+                                    ""+
+                                "</td>"+
+                                "<td>"+
+                                    ""+
+                                "</td>"+
+                            "</tr>";
+            }
+
+            $("#dadosTabela").html(vlhtml);
+            $('#valorMaximoMinimo').modal('hide');           
+
+            setTimeout(function() { 
+                debugger;
+                montaTabela(totalLinhas, 'retorno', vlhtml, contador, 'proxima');                  
+            }, 1350);            
+        }
+    }
 }
+
+
+function recarregaPagin() {
+    debugger;
+    location.reload();
+}
+
+$('#valorMaximoMinimo').on('shown.bs.modal', function () {
+    $('#vlminimo').trigger('focus');   
+})
